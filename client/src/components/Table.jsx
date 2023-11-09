4076858f2cc420e23370e0b88d2a2ec03ae67d94
Andry Ariadi <andryariadi23@gmail.com>
2023-11-09 07:10:01 +0700

    add input search

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,4 +1,8 @@
 import DataTable from "react-data-table-component";
+import { BsInfoCircleFill } from "react-icons/bs";
+import { AiTwotoneEdit } from "react-icons/ai";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { useEffect, useState } from "react";
 
 const columns = [
   {
@@ -30,18 +34,56 @@ const columns = [
     name: "Action",
     cell: (row) => (
       <div className="d-flex column-gap-1">
-        <button className="btn btn-info btn-sm">Info</button>
-        <button className="btn btn-primary btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button className="btn btn-sm">
+          <BsInfoCircleFill size={20} color="#5DBEA3" />
+        </button>
+        <button className="btn btn-sm">
+          <AiTwotoneEdit size={20} color="#5783DB" />
+        </button>
+        <button className="btn btn-sm">
+          <RiDeleteBin6Fill size={20} color="#DC3545" />
+        </button>
       </div>
     ),
   },
 ];
 
 export default function TableCom({ users }) {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearch(event) {
+    const searchValue = event.target.value.toLowerCase();
+    setSearchText(searchValue);
+  }
+
+  const filteredUsers = users.filter((user) => {
+    return user.name.toLowerCase().includes(searchText);
+  });
+
+  const tableHeaderStyle = {
+    headCells: {
+      style: {
+        fontWeight: "bold",
+        fontSize: "14px",
+        backgroundColor: "#776B5D",
+      },
+    },
+  };
+
   return (
     <div className="table-container">
-      <DataTable columns={columns} data={users} pagination fixedHeader selectableRows selectableRowsHighlight highlightOnHover />
+      <DataTable
+        columns={columns}
+        data={filteredUsers} // Gunakan data yang sudah difilter
+        customStyles={tableHeaderStyle}
+        pagination
+        fixedHeader
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        subHeader
+        subHeaderComponent={<input type="text" className="form-control w-25" placeholder="Search..." onChange={handleSearch} />}
+      />
     </div>
   );
 }
